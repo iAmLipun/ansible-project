a92909173b301dc211b9620e1124a3b0d7414aa2
iAmLipun <radiant.rudra@outlook.com>
2019-09-14 06:08:23 +0000

    first commit

---
+++ create_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Costumer (
+    CustomerName INT,
+    ContactName VARCHAR (255) NOT NULL,
+    Address VARCHAR (50),
+    City VARCHAR (30),
+    PostalCode VARCHAR (10)
+	Country VARCHAR (10)
+); 
---
+++ insert_data.sql
@@ -0,0 +1,2 @@
+INSERT INTO Customer (CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUES ('Cardinal', 'Tom B. Erichsen', 'Skagen 21', 'Stavanger', '4006', 'Norway');
